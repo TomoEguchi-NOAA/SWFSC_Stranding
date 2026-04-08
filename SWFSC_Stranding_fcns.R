cf65cf712a9480007c09683492eb36f188d0c699
Tomo Eguchi <Tomo.Eguchi@nmfs.local>
2026-04-08 09:07:01 -0700

    Changed the connection to Etrella from the temporary Estrella-ut

--- SWFSC_Stranding_fcns.R
+++ SWFSC_Stranding_fcns.R
@@ -3,9 +3,11 @@
 # Functions common to many scripts in the SWFSC_Stranding project
 
 connection.string <- function(database){
-  # return(paste0("Driver={ODBC Driver 18 for SQL Server};Server=swc-estrella-s;Database=",
-  #               database, ";Trusted_Connection=yes;TrustServerCertificate=yes;"))
+  return(paste0("Driver={ODBC Driver 18 for SQL Server};Server=swc-estrella-s;Database=",
+                database, ";Trusted_Connection=yes;TrustServerCertificate=yes;"))
   
-  return(paste0("Driver={ODBC Driver 18 for SQL Server};Server=swc-estrella-ut.nmfs.local;Database=",
-                database, ";Trusted_Connection=yes;Port=1433;TrustServerCertificate=yes;"))
-}
+  # return(paste0("Driver={ODBC Driver 18 for SQL Server};Server=swc-estrella-ut.nmfs.local;Database=",
+  #               database, ";Trusted_Connection=yes;Port=1433;TrustServerCertificate=yes;"))
+
+  }
+
